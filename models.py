aa82a916bfa03072af009d8d99f7025dab8341f9
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-12-29 11:00:46 -0700

    fix engine import issues

--- models.py
+++ models.py
@@ -1,1 +1,50 @@
-#Hold all of our classes, including the Base
+from sqlalchemy import Column, Integer, String, create_engine, func, ForeignKey
+from sqlalchemy.orm import Session, declarative_base, validates
+
+Base = declarative_base()
+
+# Consider adding an author table? An author can have many books but we'll assume a book can only have one author
+# Test add
+class Bookshelf(Base):
+    __tablename__ = "bookshelf" 
+    id = Column(Integer, primary_key = True)
+    book_title = Column(String, nullable = False)
+    book_author = Column(String, nullable = False)
+    book_description = Column(String)
+    page_count = Column(Integer)
+    # date_started = Column()
+    # date_finished = Column()
+
+    def __repr__(self):
+        return f'{self.book_title} by author {self.book_author}. Description: {self.book_description}. Page count {self.page_count}.'
+
+class WantToRead(Base):
+    __tablename__ = "want to read"
+    id = Column(Integer, primary_key = True)
+    book_title = Column(String)
+    book_author = Column(String)
+    book_description = Column(String)
+    page_count = Column(Integer)
+
+class CurrentlyReading(Base):
+    __tablename__ = "currently reading"
+    id = Column(Integer, primary_key = True)
+    book_title = Column(String)
+    book_author = Column(String)
+    book_description = Column(String)
+    page_count = Column(Integer)
+    current_page = Column(Integer)
+
+class CompletedBooks(Base):
+    __tablename__ = "completed"
+    id = Column(Integer, primary_key = True)
+    book_title = Column(String)
+    book_author = Column(String)
+    book_description = Column(String)
+    page_count = Column(Integer)
+    star_rating = Column(Integer)
+    personal_review = Column(String)
+
+
+engine = create_engine('sqlite:///bookshelf.db')
+
--- seeds.py
+++ seeds.py
@@ -1,1 +1,38 @@
-# This is our restart of database, any initial data
+from models import *
+
+# These may need to change based on how the tables get restructured to match the reqyuirements for the project
+if __name__ == '__main__':
+    Bookshelf.__table__.drop(engine)
+    WantToRead.__table__.drop(engine)
+    CurrentlyReading.__table__.drop(engine)
+    CompletedBooks.__table__.drop(engine)
+    Base.metadata.create_all(engine)
+
+    with Session(engine) as session:
+
+        b1 = Bookshelf(
+        book_title = "Hyperion",
+        book_author = "Dan Simmons",
+        book_description = "A few voyagers travel to Hyperion to solve a great cosmic mystery",
+        page_count = 500
+        )
+
+        b2 = Bookshelf(
+            book_title = "Misery",
+            book_author = "Stephen King",
+            book_description = "Crazy lady tortures novelist.",
+            page_count = 300
+        )
+    session.add_all([b1, b2])
+    session.commit()
+
+    #    all_books = session.query(Bookshelf).all()
+    #     first_book = session.query(Bookshelf).filter(Bookshelf.id ==1).first()
+    #     second_book = session.query(Bookshelf).filter(Bookshelf.id == 2).first()
+    #     # print(all_books)
+    #     print(first_book)
+    #     print(second_book)
+    #     second_book.book_description = "Crazy lady brtually tortures novelist but he ends up escaping."
+    #     session.add(second_book)
+    #     session.commit()
+    #     print(second_book)
