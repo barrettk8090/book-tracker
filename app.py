00b5cf59334630f00badd4b69cb65516666169ea
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-12-29 11:08:48 -0700

    test user input command options

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 #Our main program
+from models import * 
+
+if __name__ == '__main__':
+    with Session(engine) as session:
+        input('''
+What would you like to do?
+              
+>> Add a book to your Want to Read shelf
+>> Add a book to your currently reading list
+>> Add a book to your completed books pile
+>> View your entire bookshelf
+''')
