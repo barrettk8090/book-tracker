877a58d541ba00c4481752c1f39fd5beedf43cbd
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-01-02 13:23:55 -0700

    add more seed data and inquirer qs

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def main():
                     message = "Welcome to GooderReads! Which bookshelf would you like to view?",
                     choices = [
                         "View all of my books.",
-                        "View my want to read books.",
+                        "View or edit my want to read books.",
                         "View my currently reading books.",
                         "View my completed books.",
                         "Exit."
@@ -26,8 +26,8 @@ def main():
             response = inquirer.prompt(questions)
             if response["starter_menu"] ==   "View all of my books.":
                 view_all()
-            elif response["starter_menu"] == "View my want to read books.":
-                pass
+            elif response["starter_menu"] == "View or edit my want to read books.":
+                wtr_main()
             elif response["starter_menu"] == "View my currently reading books.":
                 pass
             elif response["starter_menu"] == "View my completed books.":
@@ -35,8 +35,40 @@ def main():
             else:
                 exit()
 
+        #View a list of all the books on your bookshelf. 
         def view_all():
             print(f"Here's a list of all the books on your bookshelf: \n{session.query(Book).all()}")
+#### WANT TO READ ####
+        def wtr_main():
+            questions = [
+                inquirer.List(
+                    "wtr_main_menu",
+                    message = "What do you want to do with your Want To Read Books?",
+                    choices = [
+                        "View all my want to read books.",
+                        "Add a book to my want to read list.",
+                        "Edit a book currently in my want to read list", 
+                        "See a list of all the authors on my want to read list.",
+                        "<-- Go Back."
+                    ]
+                )
+            ]
+            response = inquirer.prompt(questions)
+            if response["wtr_main_menu"] == "View all my want to read books.":
+                # view_all_wtr()
+                pass
+            elif response["wtr_main_menu"] == "Add a book to my want to read list.":
+                # add_wtr()
+                pass
+            elif response["wtr_main_menu"] == "Edit a book currently in my want to read list":
+                #edit_wtr_menu()
+                pass
+            elif response["wtr_main_menu"] == "See a list of all the authors on my want to read list.":
+                #wtr_authors()
+                pass
+            else:
+                main_menu()
+
 
         
         print('''
--- seeds.py
+++ seeds.py
@@ -13,7 +13,6 @@ with Session(engine) as session:
         description = "A list of books that I'm currently reading"
     )
     
-    
     wtr_shelf = Bookshelf(
         name = "Want to Read",
         description = "A list of books that I eventually want to read."
@@ -36,8 +35,69 @@ with Session(engine) as session:
         bookshelf_id = 1
     )
 
+    b2 = Book(
+        title = "Why We Sleep",
+        author = "Matthew Walker",
+        description = "Neuroscientist and sleep expert Matthew Walker provides a revolutionary exploration of sleep, explaining how it affects every aspect of our physical and mental well-being.",
+        page_count = 368,
+        pages_read = 250,
+        type = "Currently Reading",
+        star_rating = None,
+        personal_review = None,
+        bookshelf_id = 1
+    )
+
+    b3 = Book(
+        title = "Wool",
+        author = "Hugh Howey",
+        description = "Thousands of people live underground and no one knows why.",
+        page_count = 594,
+        pages_read = 0,
+        type = "Want To Read",
+        star_rating = None,
+        personal_review = None,
+        bookshelf_id = 2
+    )
+
+    b4 = Book(
+        title = "Leviathan Wakes",
+        author = "James S.A. Corey",
+        description = "I'm honestly not entirely sure but I've heard its good.",
+        page_count = 592,
+        pages_read = 0,
+        type = "Want To Read",
+        star_rating = None,
+        personal_review = None,
+        bookshelf_id = 2
+    )
+
+    b5 = Book(
+        title = "The Three Body Problem",
+        author = "Liu Cixin",
+        description = "The people of Earth begin a friendly conversation with aliens!",
+        page_count = 472,
+        pages_read = 472,
+        type = "Completed",
+        star_rating = 5,
+        personal_review = "Completely fantastic",
+        bookshelf_id = 3
+    )
+
+    b6 = Book(
+        title = "The Dark Forest",
+        author = "Liu Cixin",
+        description = "The people of earth realize that the aliens are actually not friendly at all and want to kill everyone.",
+        page_count = 512,
+        pages_read = 512,
+        type = "Completed",
+        star_rating = 5,
+        personal_review = "This book rocked and one of the end sequences is incredible. Love the idea of the dark forest.",
+        bookshelf_id = 3
+    )
+
+
     session.add_all([cr_shelf,wtr_shelf,completed_shelf])
     session.commit()
-    session.add(b1)
+    session.add_all([b1,b2,b3,b4,b5,b6])
     session.commit()
 
